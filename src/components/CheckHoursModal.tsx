ecb059906bac548ed43417ce7358c6bd0b43f4b8
Andrew Dassonville <dassonville.andrew@gmail.com>
2023-01-01 11:01:49 -0800

    Remove unused style files

--- src/components/CheckHoursModal.tsx
+++ src/components/CheckHoursModal.tsx
@@ -41,7 +41,7 @@ export default function CheckHoursModal(props: { open: boolean, name: string, ho
                   </div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                      {name}'s Hours
+                      {name}'s Time Statistics
                     </Dialog.Title>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
-import "../styles/style.css";
-import "../styles/App.css";
 
-
-
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-full">
